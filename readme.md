e70106bf9a97348d64ed7dece5519d54fcbe153b
Rayssa <rayreisog05@gmai.com>
2025-03-13 16:02:13 -0300

    teste

---
+++ readme.md
@@ -0,0 +1,18 @@
+# Documentação do apricativo - Tratamento de erro
+
+## Inrodução
+
+### Este código em python na versão 3.12, demonstra tratamento de erros <br>
+### e também laços de repetição
+
+## Propriedade
+
+Este código foi desenvolvido por: **Rayssa** <br>
+Orientado pelo professor: **Thiago Araujo** <br>
+Local: *Senai 'Suzana Dias'* <br>
+Data: *13 de março de 2025*.
+
+## Entre em contato
+
+Para saber mais acesse: [Armazém de Bytes](https://www.uol.com.br/)
+
---
+++ tratamentoErro.py
@@ -0,0 +1,36 @@
+
+def linha():
+    print('___________________________________')
+
+
+#estratégia de implementação com laço de repetção while true
+
+while True:
+
+    #Tratamento de erro
+    try:
+        linha()
+        print('Bem vindos á calculadora de divisão!!!')
+        linha()
+        print()
+
+
+        n1 = int(input('Digite um número: '))
+        n2 = int(input('Digite outro número: '))
+
+        conta = n1 / n2
+    
+    except ZeroDivisionError:
+        linha()
+        print('não é possível dividir nada por 0')
+        linha()
+    except ValueError:
+        linha()
+        print('por favor, digite apenas números inteiros')
+        linha()
+
+    else:
+        print(f'{n1} dividido por {n2} é = {conta}')
+        break
+    
+
